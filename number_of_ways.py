bdbacb62a9be6cd2dfc7ac520f5052b7e106c579
Rama <41026094+raghavrama21@users.noreply.github.com>
2025-05-21 22:54:41 -0400

    Fixed the error in number_of_ways.py

--- number_of_ways.py
+++ number_of_ways.py
@@ -5,18 +5,18 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     """
     Solving Leetcode Problem.
     https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps/
-    
+
     Given two positive integers startPos and endPos
     Initially, you are standing at position startPos on an infinite
     number line. With one step, you can move either one position to the left,
     or one position to the right.
-    
+
     Given a positive integer k, return the number of different ways to
     reach the position endPos starting from startPos, such that you
     perform exactly k steps.
     """
     # start with path of length 1
-    paths = [startPos]
+    paths = [[startPos]]
 
     # loop k times
     for i in tqdm(range(k)):
@@ -58,6 +58,7 @@ def test_number_of_ways():
     It can be proven that no other way is possible, so we return 3.
     """
     print(numberOfWays(1, 2, 3))
+
 
 if __name__ == "__main__":
     test_number_of_ways()
